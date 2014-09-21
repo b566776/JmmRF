3a2baad43f90f61dc971f68331759f2f34e232d4
b566776 <b566776@gmail.com>
2014-09-21 14:40:32 -0300

    versao inicical

---
+++ Comando.cpp
@@ -0,0 +1,26 @@
+#include "Comando.h"
+
+// Constructors/Destructors
+//  
+
+Comando::Comando ( ) {
+initAttributes();
+}
+
+Comando::~Comando ( ) { }
+
+//  
+// Methods
+//  
+
+
+// Accessor methods
+//  
+
+
+// Other methods
+//  
+
+void Comando::initAttributes ( ) {
+}
+
---
+++ MasterNode.h
@@ -0,0 +1,27 @@
+/*
+ * MasterNode.h
+ *
+ *  Created on: 30/07/2014
+ *      Author: usuario
+ */
+
+#ifndef MASTERNODE_H_
+#define MASTERNODE_H_
+
+#include "Node.h"
+#include "WebServer.h"
+
+
+
+class MasterNode : public Node
+{
+	public:
+		void MasterNode();
+	    WebServer webserver;
+	    StackArray pilha;
+
+		void setup();
+		void loop();
+};
+
+#endif /* MASTERNODE_H_ */
